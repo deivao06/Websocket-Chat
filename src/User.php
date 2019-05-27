d97ca7e2727c36842b5380a72df049f6539a89fd
David Wesley <david.moura12@hotmail.com>
2019-05-27 16:42:12 -0300

    teste

--- src/User.php
+++ src/User.php
@@ -15,5 +15,12 @@ class User {
         $this->db = new Connection;
     }
 
-    
+    public function registerUser($name,$pass,$admin = 0){
+        $query = "INSERT INTO user (name,pass,admin) values (?,?,?)";
+        $prepare = $this->db->prepare($query);
+        $execute = $this->db->execute($prepare);
+
+        $fetch = $this->db->fetch($prepare);
+
+    }
 }
--- storage/query.sql
+++
@@ -1,7 +0,0 @@
--- CREATE TABLE users(
---     id INTEGER PRIMARY KEY,
---     name VARCHAR(250),
---     pass TEXT,
---     admin INTEGER
--- );
-UPDATE users set admin = 1 where id = 1;
