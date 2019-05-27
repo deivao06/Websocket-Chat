1cb6a69f8b4fafe287e247ec2e69315eec24f564
David Wesley <david.moura12@hotmail.com>
2019-05-27 16:44:28 -0300

    teste

--- src/User.php
+++ src/User.php
@@ -17,10 +17,9 @@ class User {
 
     public function registerUser($name,$pass,$admin = 0){
         $query = "INSERT INTO user (name,pass,admin) values (?,?,?)";
-        $prepare = $this->db->prepare($query);
-        $execute = $this->db->execute($prepare);
+        $prepare = $this->db->connection->prepare($query);
+        $execute = $prepare->execute($prepare);
 
-        $fetch = $this->db->fetch($prepare);
-
+        return $execute;
     }
 }
